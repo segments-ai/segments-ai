ad6bc3402c2131677c8612d796c7746e44f50bd9
Bert De Brabandere <db.bert@gmail.com>
2020-03-06 15:38:09 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'segments-ai',         # How you named your package folder (MyLib)
   packages = ['segments'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license = 'MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = '',   # Give a short description about your library
   author = 'Segments.ai',                   # Type in your name
