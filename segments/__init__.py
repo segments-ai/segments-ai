dfce7bf01d74ebea4c3b702db891094cfe31c5d7
Bert De Brabandere <db.bert@gmail.com>
2020-07-26 18:48:31 +0200

    Updates

--- segments/__init__.py
+++ segments/__init__.py
@@ -1,6 +1,8 @@
-from .client import SegmentsClient #, SegmentsDataset
+from .client import SegmentsClient
+from .dataset import SegmentsDataset
+from .utils import load_image, load_segmentation_bitmap
 
 __all__ = [
     'SegmentsClient',
-    # 'SegmentsDataset'
+    'SegmentsDataset',
 ]
--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 setup(
   name = 'segments-ai',         # How you named your package folder (MyLib)
   packages = ['segments'],   # Chose the same as "name"
-  version = '0.8',      # Start with a small number and increase it with every change you make
+  version = '0.9',      # Start with a small number and increase it with every change you make
   license = 'MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = '',   # Give a short description about your library
   author = 'Segments.ai',                   # Type in your name
   author_email = 'bert@segments.ai',      # Type in your E-Mail
   url = 'https://github.com/segments-ai/segments-ai',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/segments-ai/segments-ai/archive/v0.8.tar.gz',
+  download_url = 'https://github.com/segments-ai/segments-ai/archive/v0.9.tar.gz',
   keywords = ['image', 'segmentation', 'labeling', 'vision'],   # Keywords that define your package best
-  install_requires=["numpy", "requests"],
+  install_requires=["numpy", "requests", "pycocotools", "Pillow", "tqdm"],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
