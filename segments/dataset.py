f865fa85bb2d9bf9d7d85c42e0280ba5345ab2ca
Bert De Brabandere <db.bert@gmail.com>
2020-08-06 10:58:04 +0200

    Better folder structure and names for cached images and labels

--- segments/dataset.py
+++ segments/dataset.py
@@ -10,11 +10,11 @@ from PIL import Image
 
 class SegmentsDataset():
     # https://stackoverflow.com/questions/682504/what-is-a-clean-pythonic-way-to-have-multiple-constructors-in-python
-    def __init__(self, release_file, task='segmentation', filter_by=None, image_dir='images'):
+    def __init__(self, release_file, task='segmentation', filter_by=None, segments_dir='segments'):
         self.task = task
         self.filter_by = [filter_by] if isinstance(filter_by, str) else filter_by
         self.filter_by = [s.lower() for s in self.filter_by]
-        self.image_dir = image_dir
+        self.segments_dir = segments_dir
 
         
         # if urlparse(release_file).scheme in ('http', 'https'): # If it's a url
@@ -26,6 +26,9 @@ class SegmentsDataset():
             content = requests.get(release_file)
             self.release = json.loads(content.content)        
         self.release_file = release_file
+
+        self.dataset_identifier = '{}_{}'.format(self.release['dataset']['owner'], self.release['dataset']['name'])
+        self.image_dir = os.path.join(segments_dir, self.dataset_identifier)
 
         # First some checks
         if not self.task in self.release['dataset']['tasks']:
@@ -68,10 +71,12 @@ class SegmentsDataset():
 
         print('Initialized dataset with {} images.'.format(len(self)))
         
-    def _load_image_from_cache(self, sample):            
+    def _load_image_from_cache(self, sample):
+        sample_name = os.path.splitext(sample['name'])[0]
         image_url = sample['attributes']['image']['url']
         url_extension = os.path.splitext(urlparse(image_url).path)[1]
-        image_filename_rel = '{}{}'.format(sample['uuid'], url_extension)
+        # image_filename_rel = '{}{}'.format(sample['uuid'], url_extension)
+        image_filename_rel = '{}{}'.format(sample_name, url_extension)
         image_filename = os.path.join(self.image_dir, image_filename_rel)
         if os.path.exists(image_filename):
             image = Image.open(image_filename)
@@ -80,10 +85,13 @@ class SegmentsDataset():
             image.save(image_filename)
         return image, image_filename_rel
 
-    def _load_segmentation_bitmap_from_cache(self, label):
+    def _load_segmentation_bitmap_from_cache(self, sample, task):
+        sample_name = os.path.splitext(sample['name'])[0]
+        label = sample['labels'][task]
         segmentation_bitmap_url = label['attributes']['segmentation_bitmap']['url']
         url_extension = os.path.splitext(urlparse(segmentation_bitmap_url).path)[1]
-        segmentation_bitmap_filename = os.path.join(self.image_dir, '{}{}'.format(label['uuid'], url_extension))
+        # segmentation_bitmap_filename = os.path.join(self.image_dir, '{}{}'.format(label['uuid'], url_extension))
+        segmentation_bitmap_filename = os.path.join(self.image_dir, '{}_label_{}{}'.format(sample_name, task, url_extension))
         if os.path.exists(segmentation_bitmap_filename):
             segmentation_bitmap = Image.open(segmentation_bitmap_filename)
         else:
@@ -115,7 +123,7 @@ class SegmentsDataset():
         # Load the label
         if sample['labels'][self.task] is not None:
             label = sample['labels'][self.task]
-            segmentation_bitmap = self._load_segmentation_bitmap_from_cache(label)
+            segmentation_bitmap = self._load_segmentation_bitmap_from_cache(sample, self.task)
             annotations = label['attributes']['annotations']
         else:
             segmentation_bitmap = annotations = None
