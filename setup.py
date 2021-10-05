f2c0f0faf6667527840d488811a84de0723630bf
Bert De Brabandere <db.bert@gmail.com>
2021-10-05 12:28:34 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'segments-ai',         # How you named your package folder (MyLib)
   packages = ['segments'],   # Chose the same as "name"
-  version = '0.47',      # Start with a small number and increase it with every change you make
+  version = '0.48',      # Start with a small number and increase it with every change you make
   license = 'MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = '',   # Give a short description about your library
   author = 'Segments.ai',                   # Type in your name
   author_email = 'bert@segments.ai',      # Type in your E-Mail
   url = 'https://github.com/segments-ai/segments-ai',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/segments-ai/segments-ai/archive/v0.47.tar.gz',
+  download_url = 'https://github.com/segments-ai/segments-ai/archive/v0.48.tar.gz',
   keywords = ['image', 'segmentation', 'labeling', 'vision'],   # Keywords that define your package best
   install_requires = ['numpy', 'requests', 'Pillow', 'scikit-image', 'tqdm', 'pycocotools;platform_system!="Windows"', 'pycocotools-windows;platform_system=="Windows"'],
   classifiers = [
