84b30406978dcb5fe2c24bb5e118d59cf6138772
Arnout Hillen <arnout@segments.ai>
2022-04-29 23:17:24 +0200

    Update version path

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read_requirements(filename: str) -> List[str]:
 # version.py defines the VERSION and VERSION_SHORT variables.
 # We use exec here so we don't import cached_path whilst setting up.
 VERSION = {}  # type: ignore
-with open("src/segments/version.py", "r") as version_file:
+with open("segments/version.py", "r") as version_file:
     exec(version_file.read(), VERSION)
 
 setup(
