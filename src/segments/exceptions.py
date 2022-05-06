ebb42c49bc022d4c5f7a459249c303789ffcb89d
Arnout Hillen <arnout@segments.ai>
2022-05-06 17:14:11 +0200

    Add segments exceptions.

---
+++ src/segments/exceptions.py
@@ -0,0 +1,55 @@
+from typing import Optional
+
+
+class SegmentsError(Exception):
+    """Base class for exceptions."""
+
+    def __init__(self, message: str, cause: Optional[Exception] = None):
+        """
+        Args:
+            message: An informative message about the exception.
+            cause: The cause of the exception raised by Python or another library. Defaults to ``None``.
+        """
+        super().__init__(message, cause)
+        self.message = message
+        self.cause = cause
+
+    def __str__(self) -> str:
+        return self.message + str(self.args)
+
+
+class AuthenticationError(SegmentsError):
+    """Raised when an API key fails authentication."""
+
+    pass
+
+
+class AuthorizationError(SegmentsError):
+    """Raised when a user is unauthorized to perform the given request."""
+
+    pass
+
+
+class NetworkError(SegmentsError):
+    """Raised when an HTTPError occurs."""
+
+    def __init__(self, cause: Exception):
+        super().__init__(str(cause), cause)
+
+
+class TimeoutError(SegmentsError):
+    """Raised when a request times out."""
+
+    pass
+
+
+class APILimitError(SegmentsError):
+    """Raised when the user performs too many requests in a period of time."""
+
+    pass
+
+
+class ValidationError(SegmentsError):
+    """Raised when Pydantic validation of the response fails."""
+
+    pass
