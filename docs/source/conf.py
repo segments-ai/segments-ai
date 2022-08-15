0f158690c17e1aa7d7bc5c3707e92dd574aa80b4
segments-arnout <arnout@segments.ai>
2022-08-15 11:36:43 +0200

    Fix docs style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,14 +107,74 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 
 html_theme = "furo"
+common_light_dark_settings = {
+    "font-stack": "gitbook-content-font, -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif"
+}
 html_theme_options = {
     # Furo options (https://pradyunsg.me/furo/customisation/):
     # Furo will automatically add a small edit button to each document, when the documentation is generated on Read the Docs using a GitHub repository as the source.
     # "announcement": "<em>Important</em> announcement!",
     "sidebar_hide_name": True,
     "navigation_with_keys": True,
+    # https://github.com/pradyunsg/furo/blob/main/src/furo/assets/styles/variables/_colors.scss
     "light_css_variables": {
-        "font-stack": "gitbook-content-font, -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif"
+        "color-problematic": "rgb(191,53,54)",
+        # Foreground
+        "color-foreground-primary": "rgb(61,69,77)",  # for main text and headings
+        "color-foreground-secondary": "rgb(119,128,138)",  # for secondary text
+        "color-foreground-muted": "rgb(147,159,173)",  # for muted text
+        "color-foreground-border": "rgb(214,220,227)",  # for content borders
+        # Background
+        "color-background-primary": "rgb(255,255,255)",  # for content
+        "color-background-secondary": "rgb(245,247,249)",  # for navigation + ToC
+        "color-background-hover": "rgb(236,239,241)",  # for navigation-item hover
+        "color-background-hover--transparant": "rgba(236,239,241,0)",  # for navigation-item hover
+        "color-background-border": "rgb(214,220,227)",  # for UI borders
+        "color-background-item": "rgb(230,231,233)",  # for "background" items (eg: copybutton)
+        # Brand colors
+        "color-brand-primary": "rgb(90,133,237)",
+        "color-brand-content": "rgb(90,133,237)",
+        # Links
+        "color-link--hover": "rgb(44,74,152)",
+        "color-link-underline": "rgba(0,0,0,0)",
+        "color-link-underline--hover": "rgb(44,74,152)",
+        # Highlighted text (search)
+        "color-highlighted-background": "rgb(222,220,220)",
+        "color-highlighted-text": "rgb(187,214,251)",
+        # Inline code background
+        "color-inline-code-background": "rgb(245,247,249)",
+        # Admonitions
+        "color-admonition-background": "rgb(245,247,249)",
+        **common_light_dark_settings,
+    },
+    "dark_css_variables": {
+        "color-problematic": "rgb(238,130,119)",
+        # Foreground
+        "color-foreground-primary": "rgb(235,242,246)",  # for main text and headings
+        "color-foreground-secondary": "rgb(205,207,211)",  # for secondary text
+        "color-foreground-muted": "rgb(205,207,211)",  # for muted text
+        "color-foreground-border": "rgb(57,65,80)",  # for content borders
+        # Background
+        "color-background-primary": "rgb(25,28,31)",  # for content
+        "color-background-secondary": "rgb(21,23,28)",  # for navigation + ToC
+        "color-background-hover": "rgb(46,50,57)",  # for navigation-item hover
+        "color-background-border": "#rgb(45,46,56)",  # for UI borders
+        "color-background-item": "rgb(230,231,233)",  # for "background" items (eg: copybutton)
+        # Brand colors
+        "color-brand-primary": "rgb(151,175,235)",
+        "color-brand-content": "rgb(151,175,235)",
+        # Links
+        "color-link--hover": "rgb(55,97,208)",
+        "color-link-underline": "rgba(0,0,0,0)",
+        "color-link-underline--hover": "rgb(55,97,208)",
+        # Highlighted text (search)
+        "color-highlighted-background": "rgb(8,8,10)",
+        "color-highlighted-text": "rgb(63,83,116)",
+        # Inline code background
+        "color-inline-code-background": "rgb(35,39,45)",
+        # Admonitions
+        "color-admonition-background": "rgb(35,39,45)",
+        **common_light_dark_settings,
     },
 }
 pygments_style = "default"
