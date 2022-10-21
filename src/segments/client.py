8cf3d3f75ef99f7a8beb9f4464cb669f492387de
Bert De Brabandere <db.bert@gmail.com>
2022-10-21 15:12:03 +0200

    Release v1.0.12

--- src/segments/client.py
+++ src/segments/client.py
@@ -60,7 +60,7 @@ from typing_extensions import Literal
 ################################
 logger = logging.getLogger(__name__)
 T = TypeVar("T")
-VERSION = "1.0.11"
+VERSION = "1.0.12"
 
 
 ####################
