fa93faafae3ea4fa328eef9c6b929ebcef4204f8
segments-arnaud <arnaud@segments.ai>
2023-08-17 00:40:04 +0200

    small change

--- src/segments/typing.py
+++ src/segments/typing.py
@@ -12,7 +12,12 @@ class BaseModel(PydanticBaseModel):
     # TODO[pydantic]: The following keys were removed: `smart_union`.
     # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-config for more information.
     model_config = ConfigDict(
-        smart_union=True, extra="ignore", arbitrary_types_allowed=False
+        # Smart union checks all types before deciding which one to pick (otherwise, first type that fits). https://pydantic-docs.helpmanual.io/usage/model_config/#smart-union
+        smart_union=True,
+        # What happens with extra fields in dictionaries. Use ignore in production and allow in debug mode. https://pydantic-docs.helpmanual.io/usage/model_config/#change-behaviour-globally
+        extra="ignore",
+        # What happens with wrong field types. Use false in production and true in debug mode. https://pydantic-docs.helpmanual.io/usage/types/#arbitrary-types-allowed
+        arbitrary_types_allowed=False,
     )
 
 
