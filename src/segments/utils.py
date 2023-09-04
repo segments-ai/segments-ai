840d44549f8c9120c7fa841da8b822b41fea38c6
segments-arnaud <arnaud@segments.ai>
2023-09-04 12:30:29 +0200

    fix bug

--- src/segments/utils.py
+++ src/segments/utils.py
@@ -825,5 +825,5 @@ def ply_to_pcd(ply_file: str) -> None:
 
     pcd_path = ply_file.replace(".ply", ".pcd")
     # prefer RGB over intensity (tiled point cloud does not support both)
-    intensity = intensity if rgb is not None else None
+    intensity = intensity if rgb is None else None
     array_to_pcd(positions, pcd_path, intensity=intensity, rgb=rgb)
