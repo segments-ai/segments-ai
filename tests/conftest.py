a4aae2d5a15ef3ca607e29c4c43900f8dfc283ca
segments-arnaud <arnaud@segments.ai>
2023-11-16 21:03:08 +0100

    from pytest import fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,29 +3,29 @@ import json
 import os
 from typing import Iterable
 
-import pytest
+from pytest import fixture
 from segments.client import SegmentsClient
 
 
-@pytest.fixture
+@fixture
 def API_KEY() -> str:
     api_key: str = os.getenv("SEGMENTS_API_KEY")
     return api_key
 
 
-@pytest.fixture
+@fixture
 def API_URL() -> str | None:
     api_url: str | None = os.getenv("SEGMENTS_API_URL")
     return api_url
 
 
-@pytest.fixture
+@fixture
 def owner() -> str:
     dataset_owner: str = os.getenv("DATASET_OWNER")
     return dataset_owner
 
 
-@pytest.fixture
+@fixture
 def client(API_KEY: str, API_URL: str | None) -> Iterable[SegmentsClient]:
     client = (
         SegmentsClient(api_key=API_KEY, api_url=API_URL)
@@ -36,14 +36,14 @@ def client(API_KEY: str, API_URL: str | None) -> Iterable[SegmentsClient]:
     client.close()
 
 
-@pytest.fixture
+@fixture
 def datasets() -> list[str]:
     datasets: str = os.getenv("DATASETS")
     datasets: list[str] = json.loads(datasets)
     return datasets
 
 
-@pytest.fixture
+@fixture
 def sample_uuids() -> list[str]:
     """First sample uuid for each dataset."""
     sample_uuids: str = os.getenv("SAMPLE_UUIDS")
@@ -51,7 +51,7 @@ def sample_uuids() -> list[str]:
     return sample_uuids
 
 
-@pytest.fixture
+@fixture
 def sample_attribute_types() -> list[str]:
     """Sample attribute type of the datasets."""
     sample_attribute_types: str = os.getenv("SAMPLE_ATTRIBUTE_TYPES")
@@ -59,7 +59,7 @@ def sample_attribute_types() -> list[str]:
     return sample_attribute_types
 
 
-@pytest.fixture
+@fixture
 def label_attribute_types() -> list[str]:
     """Label attribute type of the datasets."""
     label_attribute_types: str = os.getenv("LABEL_ATTRIBUTE_TYPES")
@@ -67,12 +67,12 @@ def label_attribute_types() -> list[str]:
     return label_attribute_types
 
 
-@pytest.fixture
+@fixture
 def TIME_INTERVAL() -> float:
     """Wait for API call to complete."""
     return 0.2
 
 
-@pytest.fixture
+@fixture
 def ARTIFACTS_DIR() -> str:
     return "./test_artifacts"
