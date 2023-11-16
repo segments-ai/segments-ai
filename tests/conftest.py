63eec1320ab7f9b72f8ccf058181ee8f9cf21f47
segments-arnaud <arnaud@segments.ai>
2023-11-16 21:01:45 +0100

    fix errors in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import json
 import os
-from typing import Iterable, cast
+from typing import Iterable
 
 import pytest
 from segments.client import SegmentsClient
@@ -9,17 +9,20 @@ from segments.client import SegmentsClient
 
 @pytest.fixture
 def API_KEY() -> str:
-    return cast(str, os.getenv("SEGMENTS_API_KEY"))
+    api_key: str = os.getenv("SEGMENTS_API_KEY")
+    return api_key
 
 
 @pytest.fixture
 def API_URL() -> str | None:
-    return os.getenv("SEGMENTS_API_URL")
+    api_url: str | None = os.getenv("SEGMENTS_API_URL")
+    return api_url
 
 
 @pytest.fixture
 def owner() -> str:
-    return cast(str, os.getenv("DATASET_OWNER"))
+    dataset_owner: str = os.getenv("DATASET_OWNER")
+    return dataset_owner
 
 
 @pytest.fixture
@@ -35,32 +38,32 @@ def client(API_KEY: str, API_URL: str | None) -> Iterable[SegmentsClient]:
 
 @pytest.fixture
 def datasets() -> list[str]:
-    datasets_str = cast(str, os.getenv("DATASETS"))
-    datasets = cast(list[str], json.loads(datasets_str))
+    datasets: str = os.getenv("DATASETS")
+    datasets: list[str] = json.loads(datasets)
     return datasets
 
 
 @pytest.fixture
 def sample_uuids() -> list[str]:
     """First sample uuid for each dataset."""
-    sample_uuids_str = cast(str, os.getenv("SAMPLE_UUIDS"))
-    sample_uuids = cast(list[str], json.loads(sample_uuids_str))
+    sample_uuids: str = os.getenv("SAMPLE_UUIDS")
+    sample_uuids: list[str] = json.loads(sample_uuids)
     return sample_uuids
 
 
 @pytest.fixture
 def sample_attribute_types() -> list[str]:
     """Sample attribute type of the datasets."""
-    sample_attribute_types_str = cast(str, os.getenv("SAMPLE_ATTRIBUTE_TYPES"))
-    sample_attribute_types = cast(list[str], json.loads(sample_attribute_types_str))
+    sample_attribute_types: str = os.getenv("SAMPLE_ATTRIBUTE_TYPES")
+    sample_attribute_types: list[str] = json.loads(sample_attribute_types)
     return sample_attribute_types
 
 
 @pytest.fixture
 def label_attribute_types() -> list[str]:
     """Label attribute type of the datasets."""
-    label_attribute_types_str = cast(str, os.getenv("LABEL_ATTRIBUTE_TYPES"))
-    label_attribute_types = cast(list[str], json.loads(label_attribute_types_str))
+    label_attribute_types: str = os.getenv("LABEL_ATTRIBUTE_TYPES")
+    label_attribute_types: list[str] = json.loads(label_attribute_types)
     return label_attribute_types
 
 
