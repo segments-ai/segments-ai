60dbea078cb2e19b44d8e6fe8d46b399840fb963
Tom Roussel <tom.roussel@segments.ai>
2025-06-16 18:05:02 +0200

    Updated our build configuration to be fully pyproject based

--- src/segments/__init__.py
+++ src/segments/__init__.py
@@ -1,5 +1,6 @@
 from segments.client import SegmentsClient
 from segments.dataset import SegmentsDataset
+from segments.version import __version__
 
 
-__all__ = ["SegmentsClient", "SegmentsDataset"]
+__all__ = ["SegmentsClient", "SegmentsDataset", "__version__"]
